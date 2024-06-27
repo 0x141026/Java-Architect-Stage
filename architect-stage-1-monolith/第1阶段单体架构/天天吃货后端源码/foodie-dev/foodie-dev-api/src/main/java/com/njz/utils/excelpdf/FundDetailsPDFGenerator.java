53f6a7c44867869c3fc112b8ea30d00e93bab2dd
Laptop-XiaoXin-Aaron <njz4062@gmail.com>
2024-06-28 01:20:02 +0800

    refactor openpdf test

--- architect-stage-1-monolith/第1阶段单体架构/天天吃货后端源码/foodie-dev/foodie-dev-api/src/main/java/com/njz/utils/excelpdf/FundDetailsPDFGenerator.java
+++ architect-stage-1-monolith/第1阶段单体架构/天天吃货后端源码/foodie-dev/foodie-dev-api/src/main/java/com/njz/utils/excelpdf/FundDetailsPDFGenerator.java
@@ -2,6 +2,7 @@ package com.njz.utils.excelpdf;
 
 import com.lowagie.text.*;
 import com.lowagie.text.pdf.*;
+import com.njz.utils.excelpdf.dto.FundDetail;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -15,23 +16,16 @@ public class FundDetailsPDFGenerator extends PdfPageEventHelper{
     private BaseFont baseFont;
     private PdfPTable table;
     private PdfTemplate total;
-    private final float footHeight = 40; //canva绘制后的图形
+    private float footHeight;
     private final float offSetY = 3; //canva绘制后的图形
 
-    private int pageNumber = 1;
-    public FundDetailsPDFGenerator() {
+    public FundDetailsPDFGenerator(BaseFont baseFont, Font titleFont, Font paragrapyFont, PdfPTable table, float footHeight) {
         // 初始化字体
-        try {
-            baseFont = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.EMBEDDED);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        titleFont = new Font(baseFont, 20);
-        paragrapyFont = new Font(baseFont, 10);
-        table = new PdfPTable(5);
-        table.setWidthPercentage(100);
-        table.setSpacingBefore(10f);
-
+        this.baseFont = baseFont;
+        this.titleFont = titleFont;
+        this.paragrapyFont = paragrapyFont;
+        this.table = table;
+        this.footHeight = footHeight;
     }
 
     public Font getParagrapyFont() {
@@ -64,6 +58,18 @@ public class FundDetailsPDFGenerator extends PdfPageEventHelper{
 
     public void setTable(PdfPTable table) {
         this.table = table;
+    }
+
+    /**
+     * 在文档打开时调用。这是在写入内容之前第一次调用Document.open()时触发的。
+     * 可以用于初始化全局资源，如全局字体或样式设置。
+     * @param writer
+     * @param document
+     */
+    @Override
+    public void onOpenDocument(PdfWriter writer, Document document) {
+        // 文档打开时调用，创建表格
+        total = writer.getDirectContent().createTemplate(60, 18);
     }
     @Override
     public void onStartPage(PdfWriter writer, Document document) {
@@ -101,12 +107,7 @@ public class FundDetailsPDFGenerator extends PdfPageEventHelper{
 
         cb.addTemplate(total, center, document.bottom() - paragrapyFont.getSize() - offSetY - footHeight);
     }
-    @Override
-    public void onOpenDocument(PdfWriter writer, Document document) {
-        // 文档打开时调用，创建表格
-        total = writer.getDirectContent().createTemplate(60, 18);
-        addTableHeader(table);
-    }
+
     @Override
     public void onCloseDocument(PdfWriter writer, Document document) {
         // 文档关闭时调用，设置总页数
@@ -117,92 +118,7 @@ public class FundDetailsPDFGenerator extends PdfPageEventHelper{
         total.showText(totalPageString);
         total.endText();
     }
-
-    public PdfPCell createRightAlignedCell(String content) {
-        Phrase phrase = new Phrase(content, paragrapyFont);
-        PdfPCell cell = new PdfPCell(phrase);
-        cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
-        return cell;
-    }
-    // 辅助方法：创建一个自动换行的 PdfPCell
-    private PdfPCell createWrappingCell(String content) {
-        Phrase phrase = new Phrase(content, paragrapyFont);
-        PdfPCell cell = new PdfPCell(phrase);
-        cell.setNoWrap(false);
-        return cell;
-    }
-    public static float cmToPoints(float cm) {
-        return cm * 28.35f;
-    }
-
-    public static void main(String[] args) {
-        FundDetailsPDFGenerator fundDetailsPDFGenerator = new FundDetailsPDFGenerator();
-        fundDetailsPDFGenerator.generate();
-    }
-    public void generate() {
-        // 指定文件保存的路径
-        String filePath = System.getProperty("java.io.tmpdir") + "资金明细详情.pdf";
-        float marginPoint = cmToPoints(2.0f);
-        Document document = new Document(PageSize.A4.rotate(), marginPoint, marginPoint, marginPoint, marginPoint + footHeight);
-        try {
-            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(filePath));
-            // 在打开文档之前设置页脚事件
-            FundDetailsPDFGenerator event = new FundDetailsPDFGenerator();
-            writer.setPageEvent(event);
-            document.open();
-
-            // 添加标题
-            Paragraph title = new Paragraph("资金明细详情", titleFont);
-            title.setAlignment(Element.ALIGN_CENTER);
-            document.add(title);
-
-            // 添加查询日期和查询人
-            document.add(new Paragraph("查询日期：" + new Date().toString(), paragrapyFont));
-            document.add(new Paragraph("查询人：小红", paragrapyFont));
-
-            // 创建表格并设置默认单元格样式
-            PdfPTable table = new PdfPTable(5); // 5 列的表格
-            table.setWidthPercentage(100); // 表格宽度为页面宽度的 100%
-            table.setSpacingBefore(10f); // 表格前的间距
-
-            // 设置余额列的默认单元格样式为右对齐
-            PdfPCell defaultBalanceCell = new PdfPCell();
-            defaultBalanceCell.setHorizontalAlignment(Element.ALIGN_RIGHT);
-//            table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_RIGHT);
-
-            // 添加表头
-            addTableHeader(table);
-
-            // 填充表格数据
-            List<FundDetail> fundDetailsList = getFundDetailsList(); // 获取资金明细实体的列表
-            for (FundDetail fundDetail : fundDetailsList) {
-                // 检查是否有足够的空间添加新行，如果没有，则结束当前页并开始新页
-                if (table.getTotalHeight() + table.getRowHeight(1)  > document.top() - document.bottom() - footHeight) {
-                    document.add(table);
-                    document.newPage();
-                    table.deleteBodyRows(); // 清除已添加的行，以便在新页上重新开始
-                    event.pageNumber++;
-                }
-
-                table.addCell(this.createWrappingCell(fundDetail.getTransferDate()));
-                table.addCell(this.createWrappingCell(fundDetail.getPayer()));
-                table.addCell(this.createWrappingCell(fundDetail.getPayerAccount()));
-                table.addCell(this.createWrappingCell(fundDetail.getPayeeAccount()));
-
-                // 使用默认样式添加余额单元格
-                PdfPCell cell = this.createRightAlignedCell(String.valueOf(fundDetail.getBalance()));
-                table.addCell(cell);
-            }
-
-            document.add(table);
-            document.close();
-            writer.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private  void addTableHeader(PdfPTable table) {
+    private void addTableHeader(PdfPTable table) {
         PdfPCell cell = this.createWrappingCell("转账日期");
         PdfPCell cell1 = this.createWrappingCell("转出人");
         PdfPCell cell2 = this.createWrappingCell("转出账号");
@@ -218,72 +134,72 @@ public class FundDetailsPDFGenerator extends PdfPageEventHelper{
         table.addCell(balanceHeader);
     }
 
-    private static List<FundDetail> getFundDetailsList() {
-        // 这里应该是从数据库或其他数据源获取资金明细实体的列表
-        // 返回填充好的资金明细实体列表
-        List<FundDetail> fundDetailList = new ArrayList<FundDetail>();
-        for(int i = 0; i < 100; ++i) {
-            FundDetail fundDetail = new FundDetail("2024010" + i, "付款人" + i, "" + i, "收款人账号" + i, 100 + i);
-            fundDetailList.add(fundDetail);
-        }
-        return fundDetailList; // 示例代码，需要替换为实际的数据获取逻辑
+    private PdfPCell createWrappingCell(String content) {
+        Phrase phrase = new Phrase(content, paragrapyFont);
+        PdfPCell cell = new PdfPCell(phrase);
+        cell.setNoWrap(false);
+        return cell;
     }
+//    public static void main(String[] args) {
+//        FundDetailsPDFGenerator fundDetailsPDFGenerator = new FundDetailsPDFGenerator();
+//        fundDetailsPDFGenerator.generate();
+//    }
+//    public void generate() {
+//        // 指定文件保存的路径
+//        String filePath = System.getProperty("java.io.tmpdir") + "资金明细详情.pdf";
+//        float marginPoint = cmToPoints(2.0f);
+//        Document document = new Document(PageSize.A4.rotate(), marginPoint, marginPoint, marginPoint, marginPoint + footHeight);
+//        try {
+//            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(filePath));
+//            // 在打开文档之前设置页脚事件
+//            FundDetailsPDFGenerator event = new FundDetailsPDFGenerator();
+//            writer.setPageEvent(event);
+//            document.open();
+//
+//            // 添加标题
+//            Paragraph title = new Paragraph("资金明细详情", titleFont);
+//            title.setAlignment(Element.ALIGN_CENTER);
+//            document.add(title);
+//
+//            // 添加查询日期和查询人
+//            document.add(new Paragraph("查询日期：" + new Date().toString(), paragrapyFont));
+//            document.add(new Paragraph("查询人：小红", paragrapyFont));
+//
+//            // 创建表格并设置默认单元格样式
+////            PdfPTable table = new PdfPTable(5); // 5 列的表格
+////            table.setWidthPercentage(100); // 表格宽度为页面宽度的 100%
+////            table.setSpacingBefore(10f); // 表格前的间距
+//
+//            // 设置余额列的默认单元格样式为右对齐
+//            PdfPCell defaultBalanceCell = new PdfPCell();
+//            defaultBalanceCell.setHorizontalAlignment(Element.ALIGN_RIGHT);
+////            table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_RIGHT);
+//
+//            // 添加表头
+//            addTableHeader(table);
+//
+//            // 填充表格数据
+//            List<FundDetail> fundDetailsList = getFundDetailsList(); // 获取资金明细实体的列表
+//            for (FundDetail fundDetail : fundDetailsList) {
+//                table.addCell(this.createWrappingCell(fundDetail.getTransferDate()));
+//                table.addCell(this.createWrappingCell(fundDetail.getPayer()));
+//                table.addCell(this.createWrappingCell(fundDetail.getPayerAccount()));
+//                table.addCell(this.createWrappingCell(fundDetail.getPayeeAccount()));
+//
+//                // 使用默认样式添加余额单元格
+//                PdfPCell cell = this.createRightAlignedCell(String.valueOf(fundDetail.getBalance()));
+//                table.addCell(cell);
+//            }
+//
+//            document.add(table);
+//            document.close();
+//            writer.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
-    // 资金明细实体类
-    public static class FundDetail {
-        private String transferDate;
-        private String payer;
-        private String payerAccount;
-        private String payeeAccount;
-        private double balance;
 
-        // 构造函数、getter 和 setter 省略
-        public FundDetail(String transferDate, String payer, String payerAccount, String payeeAccount, double balance) {
-            this.transferDate = transferDate;
-            this.payer = payer;
-            this.payerAccount = payerAccount;
-            this.payeeAccount = payeeAccount;
-            this.balance = balance;
-        }
 
-        public String getTransferDate() {
-            return transferDate;
-        }
 
-        public void setTransferDate(String transferDate) {
-            this.transferDate = transferDate;
-        }
-
-        public String getPayer() {
-            return payer;
-        }
-
-        public void setPayer(String payer) {
-            this.payer = payer;
-        }
-
-        public String getPayerAccount() {
-            return payerAccount;
-        }
-
-        public void setPayerAccount(String payerAccount) {
-            this.payerAccount = payerAccount;
-        }
-
-        public String getPayeeAccount() {
-            return payeeAccount;
-        }
-
-        public void setPayeeAccount(String payeeAccount) {
-            this.payeeAccount = payeeAccount;
-        }
-
-        public double getBalance() {
-            return balance;
-        }
-
-        public void setBalance(double balance) {
-            this.balance = balance;
-        }
-    }
 }
--- architect-stage-1-monolith/第1阶段单体架构/天天吃货后端源码/foodie-dev/foodie-dev-api/src/main/java/com/njz/utils/excelpdf/Test.java
+++ architect-stage-1-monolith/第1阶段单体架构/天天吃货后端源码/foodie-dev/foodie-dev-api/src/main/java/com/njz/utils/excelpdf/Test.java
@@ -1,7 +1,123 @@
 package com.njz.utils.excelpdf;
 
-public class Test {
-    public static void main(String[] args) {
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.BaseFont;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+import com.njz.utils.excelpdf.dto.FundDetail;
 
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Test {
+    private final float footHeight = 40; //canva绘制后的图形
+
+    public static float cmToPoints(float cm) {
+        return cm * 28.35f;
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.generate();
+    }
+    public PdfPCell createRightAlignedCell(String content, FundDetailsPDFGenerator event) {
+        Phrase phrase = new Phrase(content, event.getParagrapyFont());
+        PdfPCell cell = new PdfPCell(phrase);
+        cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
+        return cell;
+    }
+    // 辅助方法：创建一个自动换行的 PdfPCell
+    private PdfPCell createWrappingCell(String content, Font font) {
+        Phrase phrase = new Phrase(content, font);
+        PdfPCell cell = new PdfPCell(phrase);
+        cell.setNoWrap(false);
+        return cell;
+    }
+    public void generate() {
+        // 指定文件保存的路径
+        String filePath = System.getProperty("java.io.tmpdir") + "资金明细详情.pdf";
+        float marginPoint = cmToPoints(2.0f);
+        Document document = new Document(PageSize.A4.rotate(), marginPoint, marginPoint, marginPoint, marginPoint + footHeight);
+        try {
+            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(filePath));
+            // 在打开文档之前设置页脚事件
+            BaseFont baseFont = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.EMBEDDED);
+            Font titleFont = new Font(baseFont, 20);
+            Font paragrapyFont = new Font(baseFont, 10);
+            // 创建表格并设置默认单元格样式
+            PdfPTable table = new PdfPTable(5);// 5列的表格
+            table.setWidthPercentage(100);// 表格宽度为页面宽度的 100%
+            table.setSpacingBefore(10f);// 表格前的间距
+            addTableHeader(table, paragrapyFont);
+            FundDetailsPDFGenerator event = new FundDetailsPDFGenerator(baseFont, titleFont, paragrapyFont, table, footHeight);
+            writer.setPageEvent(event);
+            document.open();
+
+            // 添加标题
+            Paragraph title = new Paragraph("资金明细详情", event.getTitleFont());
+            title.setAlignment(Element.ALIGN_CENTER);
+            document.add(title);
+
+            // 添加查询日期和查询人
+            document.add(new Paragraph("查询日期：" + new Date().toString(), event.getParagrapyFont()));
+            document.add(new Paragraph("查询人：小红", event.getParagrapyFont()));
+
+            // 设置余额列的默认单元格样式为右对齐
+            PdfPCell defaultBalanceCell = new PdfPCell();
+            defaultBalanceCell.setHorizontalAlignment(Element.ALIGN_RIGHT);
+//            table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_RIGHT);
+
+            // 添加表头
+            addTableHeader(table, paragrapyFont);
+
+            // 填充表格数据
+            List<FundDetail> fundDetailsList = getFundDetailsList(); // 获取资金明细实体的列表
+            for (FundDetail fundDetail : fundDetailsList) {
+                table.addCell(this.createWrappingCell(fundDetail.getTransferDate(), paragrapyFont));
+                table.addCell(this.createWrappingCell(fundDetail.getPayer(), paragrapyFont));
+                table.addCell(this.createWrappingCell(fundDetail.getPayerAccount(), paragrapyFont));
+                table.addCell(this.createWrappingCell(fundDetail.getPayeeAccount(), paragrapyFont));
+
+                // 使用默认样式添加余额单元格
+                PdfPCell cell = this.createRightAlignedCell(String.valueOf(fundDetail.getBalance()), event);
+                table.addCell(cell);
+            }
+
+            document.add(table);
+            document.close();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addTableHeader(PdfPTable table, Font font) {
+        PdfPCell cell = this.createWrappingCell("转账日期", font);
+        PdfPCell cell1 = this.createWrappingCell("转出人", font);
+        PdfPCell cell2 = this.createWrappingCell("转出账号", font);
+        PdfPCell cell3 = this.createWrappingCell("转入账号", font);
+        table.addCell(cell);
+        table.addCell(cell1);
+        table.addCell(cell2);
+        table.addCell(cell3);
+
+        // 余额表头右对齐
+        PdfPCell balanceHeader = this.createWrappingCell("余额", font);
+        balanceHeader.setHorizontalAlignment(Element.ALIGN_RIGHT);
+        table.addCell(balanceHeader);
+    }
+
+    private static List<FundDetail> getFundDetailsList() {
+        // 这里应该是从数据库或其他数据源获取资金明细实体的列表
+        // 返回填充好的资金明细实体列表
+        List<FundDetail> fundDetailList = new ArrayList<FundDetail>();
+        for(int i = 0; i < 100; ++i) {
+            FundDetail fundDetail = new FundDetail("2024010" + i, "付款人" + i, "" + i, "收款人账号" + i, 100 + i);
+            fundDetailList.add(fundDetail);
+        }
+        return fundDetailList; // 示例代码，需要替换为实际的数据获取逻辑
     }
 }
